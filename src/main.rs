5bd1a1f3b2718e565c33e7e3be61670847f8ae5f
giacomopiccinini <piccinini.giacomo@gmail.com>
2025-10-14 19:52:09 +0200

    [FEAT] Add resample

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use spectrs::utils::audio::read_audio_file_mono;
 use anyhow::{Context, Result};
+use spectrs::utils::audio::{read_audio_file_mono, resample};
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let (audio, sr) = read_audio_file_mono("harvard.wav")?;
+
+    let target_sr: u32 = 16_000;
+
+    let resampled = resample(audio, sr, target_sr);
 
     Ok(())
 }
--- src/utils/audio.rs
+++ src/utils/audio.rs
@@ -1,8 +1,9 @@
 use anyhow::{Context, Result};
 use hound::WavReader;
+use rubato::{FftFixedIn, Resampler};
 
 /// Read audio file from file path and convert to mono by averaging left and right channel
-pub fn read_audio_file_mono(audio_file_path: &str) -> Result<(Vec<f64>, u32)> {
+pub fn read_audio_file_mono(audio_file_path: &str) -> Result<(Vec<f32>, u32)> {
     // Open the WAV file
     let mut reader =
         WavReader::open(audio_file_path).with_context(|| "Failed to open audio file")?;
@@ -22,7 +23,7 @@ pub fn read_audio_file_mono(audio_file_path: &str) -> Result<(Vec<f64>, u32)> {
     }
 
     // Init samples vec
-    let mut samples: Vec<f64> = Vec::new();
+    let mut samples: Vec<f32> = Vec::new();
 
     // Calculate the maximum value based on bits_per_sample
     let max_value = 2_f64.powi(bits_per_sample as i32 - 1);
@@ -42,14 +43,35 @@ pub fn read_audio_file_mono(audio_file_path: &str) -> Result<(Vec<f64>, u32)> {
                 acc += sample;
                 if i % 2 != 0 {
                     // Average and normalize by dividing by max_value
-                    samples.push(acc as f64 / 2.0 / max_value);
+                    samples.push(acc as f32 / 2.0 / max_value as f32);
                     acc = 0_i32;
                 }
             } else if channels == 1 {
                 // Normalize by dividing by max_value
-                samples.push(sample as f64 / max_value);
+                samples.push(sample as f32 / max_value as f32);
             }
         });
 
     Ok((samples, sr))
+}
+
+/// Resample audio file to target sample rate
+pub fn resample(samples: Vec<f32>, original_sr: u32, target_sr: u32) -> Result<Vec<f32>> {
+    // Initialize the resampler
+    let mut resampler = FftFixedIn::<f32>::new(
+        original_sr as usize,
+        target_sr as usize,
+        samples.len(), // Number of frames per channel (1 channel)
+        1024,
+        1, // Always mono by construction
+    )
+    .with_context(|| "Can't initiate resampler")?;
+
+    // Perform the resampling
+    let mut resampled = resampler
+        .process(&vec![samples], None)
+        .with_context(|| "Can't resample file")?;
+
+    // Take ownership of the first channel, avoiding cloning
+    Ok(resampled.swap_remove(0))
 }
